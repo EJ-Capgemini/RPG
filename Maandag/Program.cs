6c89c94a172b990d11e87eaa699f33ce04f31c7d
EJACOBS <bU6Am9yrRPBOSPF3aqZR>
2019-02-04 09:57:03 +0100

    initial commit

---
+++ Maandag/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Maandag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
